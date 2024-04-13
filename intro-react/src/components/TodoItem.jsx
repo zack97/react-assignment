bad39fad796893e0959d438b86d2c91286707dc7
zack10 <zacksmetz@yahoo.com>
2024-04-13 18:32:12 +0200

    todos done 3

--- intro-react/src/components/TodoItem.jsx
+++ intro-react/src/components/TodoItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TodoItem({ id, task, completed, onToggleCompleted }) {
   const handleChange = () => {
-    // onToggleCompleted(id);
+    onToggleCompleted(id);
   };
 
   return (
