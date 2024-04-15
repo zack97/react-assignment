6b65634a416fa04fe3315d8ececc35ae91d45a3b
zack10 <zacksmetz@yahoo.com>
2024-04-15 09:50:28 +0200

    todo list done original 1

--- intro-react/src/App.jsx
+++ intro-react/src/App.jsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
-import { v4 as uuidv4 } from "uuid"; // Import uuidv4 for generating unique IDs
-
-const LSKEY = 1997; // Define a key for local storage
+import { v4 as uuidv4 } from "uuid";
+const LSKEY = 1997;
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
+  const contentTodo = [];
+  const [todos, setTodos] = useState(contentTodo);
   useEffect(() => {
-    // Load todos from local storage on initial render
     const storedTodos = JSON.parse(
       window.localStorage.getItem(LSKEY + ".todos")
     );
@@ -25,7 +22,6 @@ function App() {
   };
 
   useEffect(() => {
-    // Update local storage when todos change
     window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
   }, [todos]);
 
